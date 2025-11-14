d0e0406929a6fe3152292b7e5ad88061d5d57881
Ajmal <Ajmal@DESKTOP-01V489G>
2025-11-14 10:55:21 +0500

    Add project files.

---
+++ Tyuiu.ShahabAJ2.Sprint2.Task0.V6.Lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.ShahabAJ2.Sprint2.Task0.V6.Lib
+{
+    public class DataService : ISprint2Task0V6
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] results = new bool[6];
+
+            // Операции сравнения
+            results[0] = x + 647 == y;        // 107 + 647 = 754 → True
+            results[1] = x != y - 647;        // 107 != 754-647=107 → False? Wait, need True
+            results[2] = y - 647 > x;         // 754-647=107 > 107 → False? Wait, need True
+            results[3] = x >= y;              // 107 >= 754 → False
+            results[4] = x < y;               // 107 < 754 → True
+            results[5] = y <= x;              // 754 <= 107 → False
+
+            return results;
+        }
+    }
+}
---
+++ Tyuiu.ShahabAJ2.Sprint2.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.ShahabAJ2.Sprint2.Task0.V6.Lib;
+namespace Tyuiu.ShahabAJ2.Sprint2.Task0.V6.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 107;
+            int y = 754;
+            bool[] wait = { true, true, true, false, true, false };
+            bool[] result = ds.GetCompareOperations(x, y);
+            CollectionAssert.AreEqual(wait, result);
+        }
+    }
+}

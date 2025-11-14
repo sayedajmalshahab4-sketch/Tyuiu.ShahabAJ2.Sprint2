e2741d876fbd7d318d8eefb2b4a2e15def0ab9b8
Ajmal <Ajmal@DESKTOP-01V489G>
2025-11-14 22:34:12 +0500

    Task6

---
+++ Tyuiu.ShahabAJ2.Sprint2.Task6.V12.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint1;
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.ShahabAJ2.Sprint2.Task6.V12.Lib
+{
+    public class DataService : ISprint2Task5V12
+    {
+        public string FindDateOfPreviousDay(int g, int m, int n)
+        {
+            int prevDay = n - 1;
+            int prevMonth = m;
+            int prevYear = g;
+
+            if (prevDay == 0)
+            {
+                prevMonth--;
+
+                if (prevMonth == 0)
+                {
+                    prevMonth = 12;
+                    prevYear--;
+                }
+
+                // Использование сокращенной формы оператора switch
+                prevDay = prevMonth switch
+                {
+                    1 or 3 or 5 or 7 or 8 or 10 or 12 => 31,
+                    4 or 6 or 9 or 11 => 30,
+                    2 => 29, // Високосный год
+                    _ => 30
+                };
+            }
+
+            // Форматирование с нулями для однозначных чисел
+            string formattedDay = prevDay.ToString().PadLeft(2, '0');
+            string formattedMonth = prevMonth.ToString().PadLeft(2, '0');
+
+            return $"{formattedDay}.{formattedMonth}.{prevYear}";
+        }
+    }
+}
---
+++ Tyuiu.ShahabAJ2.Sprint2.Task6.V12.Test/DataServiceTest.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.ShahabAJ2.Sprint2.Task6.V12.Lib;
+
+namespace Tyuiu.ShahabAJ2.Sprint2.Task6.V12.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidFindDateOfPreviousDay1()
+        {
+            DataService ds = new DataService();
+            int g = 2024;
+            int m = 9;
+            int n = 8;
+            string wait = "07.09.2024";
+            string result = ds.FindDateOfPreviousDay(g, m, n);
+            Assert.AreEqual(wait, result);
+        }
+
+        [TestMethod]
+        public void ValidFindDateOfPreviousDay2()
+        {
+            DataService ds = new DataService();
+            int g = 2024;
+            int m = 1;
+            int n = 1;
+            string wait = "31.12.2023";
+            string result = ds.FindDateOfPreviousDay(g, m, n);
+            Assert.AreEqual(wait, result);
+        }
+
+        [TestMethod]
+        public void ValidFindDateOfPreviousDay3()
+        {
+            DataService ds = new DataService();
+            int g = 2024;
+            int m = 3;
+            int n = 1;
+            string wait = "29.02.2024";
+            string result = ds.FindDateOfPreviousDay(g, m, n);
+            Assert.AreEqual(wait, result);
+        }
+
+        [TestMethod]
+        public void ValidFindDateOfPreviousDay4()
+        {
+            DataService ds = new DataService();
+            int g = 2024;
+            int m = 5;
+            int n = 1;
+            string wait = "30.04.2024";
+            string result = ds.FindDateOfPreviousDay(g, m, n);
+            Assert.AreEqual(wait, result);
+        }
+    }
+}

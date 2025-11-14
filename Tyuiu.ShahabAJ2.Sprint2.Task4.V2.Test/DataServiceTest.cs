182075adb130a922efbe787b2d66268ab2dfddc2
Ajmal <Ajmal@DESKTOP-01V489G>
2025-11-14 21:00:32 +0500

    Task4

---
+++ Tyuiu.ShahabAJ2.Sprint2.Task4.V2.Test/DataServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.ShahabAJ2.Sprint2.Task4.V2.Lib;
+
+namespace Tyuiu.ShahabAJ2.Sprint2.Task4.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 10;
+            double y = 5;
+            // x > y + 3 → 10 > 5+3 → 10 > 8 → True
+            // z = (10 + 1) / 5 = 11 / 5 = 2.2
+            double wait = 2.2;
+            double result = ds.Calculate(x, y);
+            Assert.AreEqual(wait, result);
+        }
+
+        [TestMethod]
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 8;
+            // x > y + 3 → 2 > 8+3 → 2 > 11 → False
+            // z = (8 + 5) / (8 + 2) = 13 / 10 = 1.3
+            double wait = 1.3;
+            double result = ds.Calculate(x, y);
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
---
+++ Tyuiu.ShahabAJ2.Sprint2.Task4.V2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Tyuiu.ShahabAJ2.Sprint2.Task4.V2.Lib;
+
+namespace Tyuiu.ShahabA.Sprint2.Task4.V2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.Write("Введите значение X: ");
+            double x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите значение Y: ");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("***************************************************************************");
+
+            double result = ds.Calculate(x, y);
+            Console.WriteLine($"Значение Z = {result}");
+
+            Console.ReadKey();
+        }
+    }
+}

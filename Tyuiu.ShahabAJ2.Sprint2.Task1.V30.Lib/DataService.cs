f8b905948c4851b41f07e0cfdf17f5078b98ba77
Ajmal <Ajmal@DESKTOP-01V489G>
2025-11-26 09:01:09 +0500

    Task1

--- Tyuiu.ShahabAJ2.Sprint2.Task1.V30.Lib/DataService.cs
+++ Tyuiu.ShahabAJ2.Sprint2.Task1.V30.Lib/DataService.cs
@@ -1,24 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using tyuiu.cources.programming.interfaces.Sprint2;
+
 namespace Tyuiu.ShahabAJ2.Sprint2.Task1.V30.Lib
 {
     public class DataService : ISprint2Task1V30
-    { 
-        public DataService()
-        {
-        }
-
+    {
         public bool[] GetLogicOperations(int a, int b, int c, int d)
         {
             bool[] result = new bool[6];
 
-            // Используем все требуемые операции сравнения и логические операции 
-            result[0] = (a < b) & (c != d);           // <, !=, &
-            result[1] = (a > b) | (c == d);           // >, ==, |
-            result[2] = (a <= b) && (c < d);          // <=, <, &&
-            result[3] = (a >= c) || (b > d);          // >=, >, ||kr
-            result[4] = (a != b) ^ (c <= d);          // !=, <=, ^
-            result[5] = !(a > c) & (b == d);          // !, >, ==, &
+            // 1. (a > b) AND (c < d)
+            result[0] = (a > b) && (c < d);
+
+            // 2. (a > b) OR (c == d) 
+            result[1] = (a > b) || (c == d);
+
+            // 3. (a + 100 > b) AND (c < d)
+            result[2] = (a + 100 > b) && (c < d);
+
+            // 4. (a <= b) OR (c != d)
+            result[3] = (a <= b) || (c != d);
+
+            // 5. !(a > c) AND (b == d)
+            result[4] = !(a > c) && (b == d);
+
+            // 6. (a < b) XOR (c > d)
+            result[5] = (a < b) ^ (c > d);
 
             return result;
         }
